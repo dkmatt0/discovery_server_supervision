e92e7c96f0009b5e4a1c4bf709ee8b6e739ac0d4
dkmatt0 <dkmatt0@gmx.fr>
2019-02-20 21:47:15 +0100

    Remise en forme et correction mineur de scan.py

--- scan.py
+++ scan.py
@@ -3,95 +3,97 @@
 
 import sys
 
-from scapy.all import *
-from common import open_yaml
+from scapy.all import sr1, IP, ICMP, TCP
+from common import open_yaml, compare_ip
+
 
 class ScanIP(object):
-  """Permet différent type de scan ip a partir d'un ou plusieurs plage ip"""
-  def __init__(self):
-    self.ip = []
+    """Permet différent type de scan ip a partir d'un ou plusieurs plage ip"""
 
-  def _range_ip(self, begin, end):
-    """Fourniture d'une liste d'ip allant de begin (inclus) à end (inclus)."""
-    # Convertion en liste
-    begin = [int(x) for x in begin.split('.')]
-    end = [int(x) for x in end.split('.')]
+    def __init__(self):
+        self.ip = []
 
-    # Mise en ordre des ip (la plus petite dans begin)
-    b = ['{0:0>3}'.format(x) for x in begin]
-    e = ['{0:0>3}'.format(x) for x in end]
-    if b>e:
-      begin, end = end, begin
-    del b
-    del e
+    def _range_ip(self, begin, end):
+        """Fourniture d'une liste d'ip allant de begin (inclus) à end (inclus)."""
+        # Convertion en liste
+        begin = [int(x) for x in begin.split(".")]
+        end = [int(x) for x in end.split(".")]
 
-    # Creation de la liste d'adresse
-    list_address = []
-    while begin <= end:
-      list_address.append('.'.join([str(x) for x in begin]))
-      begin[3] += 1
-      if begin[3] >= 256:
-        begin[2] += 1
-        begin[3] = 0
-      if begin[2] >= 256:
-        begin[1] += 1
-        begin[2] = 0
-      if begin[1] >= 256:
-        begin[0] += 1
-        begin[1] = 0
-    return list_address
+        # Mise en ordre des ip (la plus petite dans begin)
+        b = ["{0:0>3}".format(x) for x in begin]
+        e = ["{0:0>3}".format(x) for x in end]
+        if b > e:
+            begin, end = end, begin
+        del b
+        del e
 
-  def add_ip(self, ip):
-    if type(ip) is not tuple: raise TypeError
-    self.ip.extend(self._range_ip(*ip))
-    return len(self.ip)
+        # Creation de la liste d'adresse
+        list_address = []
+        while begin <= end:
+            list_address.append(".".join([str(x) for x in begin]))
+            begin[3] += 1
+            if begin[3] >= 256:
+                begin[2] += 1
+                begin[3] = 0
+            if begin[2] >= 256:
+                begin[1] += 1
+                begin[2] = 0
+            if begin[1] >= 256:
+                begin[0] += 1
+                begin[1] = 0
+        return list_address
 
-  def check_ping(self, timeout=3, keep_ip=False):
-    """Fonction de ping (ICMP)."""
-    list_ip = []
-    for ip in self.ip:
-      response = sr1(IP(dst=ip) / ICMP(), timeout=timeout, verbose=False)
-      if response is not None and response[1].type == 0:
-        list_ip.append(ip)
-    if keep_ip: return list_ip
-    else:
-      self.ip = list_ip
-      len(self.ip)
+    def add_ip(self, ip):
+        if type(ip) is not tuple:
+            raise TypeError
+        self.ip.extend(self._range_ip(*ip))
+        return len(self.ip)
 
-  def check_icmp(self, *args, **kwargs):
-    return self.check_ping(*args, **kwargs)
+    def check_ping(self, timeout=3, keep_ip=False):
+        """Fonction de ping (ICMP)."""
+        list_ip = []
+        for ip in self.ip:
+            response = sr1(IP(dst=ip) / ICMP(), timeout=timeout, verbose=False)
+            if response is not None and response[1].type == 0:
+                list_ip.append(ip)
+        if keep_ip:
+            return list_ip
+        else:
+            self.ip = list_ip
+            len(self.ip)
 
-  def check_tcp(self, ip, timeout=3, keep_ip=False):
-    """Fonction de scan tcp."""
-    pass
+    def check_icmp(self, *args, **kwargs):
+        return self.check_ping(*args, **kwargs)
 
-  def get_ip(self):
-    return self.ip
+    def check_tcp(self, ip, timeout=3, keep_ip=False):
+        """Fonction de scan tcp."""
+        pass
 
-  def _ip_exist(self, ip, ip_list_supervision):
-    """Recherche une ip dans une liste d'ip et renvoie True si l'ip est trouvé."""
-    if ip in ip_list_supervision: return True
-    else: return False
+    def get_ip(self):
+        return self.ip
 
 
 ## test / debug
 
 if __name__ == "__main__":
-  ip=ScanIP()
+    ip = ScanIP()
 
-  config = open_yaml('config.yaml')
-  if config is not None:
-    if 'network' in config:
-      for config_range_ip in config['network']:
-        ip.add_ip((config_range_ip['begin'], config_range_ip['end']))
+    config = open_yaml("config.yaml")
+    if config is not None:
+        if "network" in config:
+            for config_range_ip in config["network"]:
+                ip.add_ip((config_range_ip["begin"], config_range_ip["end"]))
+        else:
+            sys.stderr.write('Le fichier yaml n\'a pas de section "network".')
+            exit(1)
     else:
-      sys.stderr.write("Le fichier yaml n'a pas de section \"network\".")
-      exit(1)
-  else:
-    sys.stderr.write("Fichier config.yaml non charge!")
-    exit(1)
+        sys.stderr.write("Fichier config.yaml non chargé!")
+        exit(1)
 
-  ip.check_ping()
-  print(ip.get_ip())
+    # ip.check_ping()
 
+    list_ip_supervision = ["192.168.10.10"]
 
+    known_ip, unknown_ip = compare_ip(ip.get_ip(), list_ip_supervision)
+
+    print(known_ip, unknown_ip)
